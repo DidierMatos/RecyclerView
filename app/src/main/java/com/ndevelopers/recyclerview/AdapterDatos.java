3873897d1366bec4bf4d82ade4623b3bf6318cb7
Didier Matos <d.matostorres@gmail.com>
2019-03-26 14:16:02 -0500

    Construyendo el adaptador

--- app/src/main/java/com/ndevelopers/recyclerview/AdapterDatos.java
+++ app/src/main/java/com/ndevelopers/recyclerview/AdapterDatos.java
@@ -2,8 +2,11 @@ package com.ndevelopers.recyclerview;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.text.Layout;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -17,23 +20,33 @@ public class AdapterDatos extends RecyclerView.Adapter<AdapterDatos.ViewHolderDa
 
     @NonNull
     @Override
-    public ViewHolderDatos onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+    public ViewHolderDatos onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) { //enlazar con itemlist
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_list,null,false);
+        return new ViewHolderDatos(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolderDatos viewHolderDatos, int i) {
-
+    public void onBindViewHolder(@NonNull ViewHolderDatos viewHolderDatos, int i) { //establece la comunicacion entre adaptador y viewholderdatos
+        viewHolderDatos.asignarDatos(listDatos.get(i));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+
+        return listDatos.size(); //retorna el tamaño de la lista
     }
 
     public class ViewHolderDatos extends RecyclerView.ViewHolder {
+
+        TextView dato;
+
         public ViewHolderDatos(@NonNull View itemView) {
             super(itemView);
+            dato = (TextView) itemView.findViewById(R.id.idDato);
+        }
+
+        public void asignarDatos(String datos) {
+            dato.setText(datos);
         }
     }
 }
