a25509a3b218d2e6d17bf9ea5579632629e2e615
Didier Matos <d.matostorres@gmail.com>
2019-03-26 14:21:39 -0500

    Ciclando y mostrando la info obtenida

--- app/src/main/java/com/ndevelopers/recyclerview/MainActivity.java
+++ app/src/main/java/com/ndevelopers/recyclerview/MainActivity.java
@@ -2,12 +2,32 @@ package com.ndevelopers.recyclerview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> listDatos; //envia como parametro
+    RecyclerView recycler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recycler = (RecyclerView) findViewById(R.id.recyclerView);
+
+        recycler.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL,false));
+
+        listDatos = new ArrayList<String>();
+
+        for(int i = 0; i < 50; i++){ //representa la info que se cargara en la lista
+            listDatos.add("Dato: # "+i+" ");
+        }
+
+        AdapterDatos adapter = new AdapterDatos(listDatos);
+        recycler.setAdapter(adapter);
     }
 }
